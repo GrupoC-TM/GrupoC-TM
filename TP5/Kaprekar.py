09aa8420ea8581cb6be75d84ecc14475c140cb9b
JoaquinRozaMorales <ignacio.roza@alumnos.frm.utn.edu.ar>
2020-05-20 15:00:07 -0300

    muestra por pantalla

--- TP5/Kaprekar.py
+++ TP5/Kaprekar.py
@@ -11,7 +11,8 @@ for x in range(cantCasos):     # iteramos de acuerdo a la cantidad de casos
     if numeroIngreso == 1111 or numeroIngreso == 2222 or numeroIngreso == 3333 or numeroIngreso ==4444 or numeroIngreso ==5555 or numeroIngreso == 6666 or numeroIngreso ==7777 or numeroIngreso == 8888 or numeroIngreso ==9999 or numeroIngreso == 0000:
         bandera2 =True   # en este caso hacemos lo mismo que en el if anterior
     numeroResta =0    #asignamos 0 a numero resta para poder hacer la resta de cada numero (en cada vuelta numeroResta se hace 0 para que borre el resultado de la iteracion anterior)
-	for i in range(10): #hacemos otro bucle for para poder encontrar la constante (sabiendo que mas de 11 iteraciones no van a ser)
+	  
+    for i in range(10): #hacemos otro bucle for para poder encontrar la constante (sabiendo que mas de 11 iteraciones no van a ser)
         numeroIngreso="{:04d}".format(numeroIngreso) #con esta funcion le damos 4 digitos al numero que ingreso el usuario (por si mete un 12, quedaría: 0012)
         numGrande = "".join(sorted(numeroIngreso,reverse=True))#con esta funcion ordenamos los digitos del numero de mayor a menor
         numChico = "".join(sorted(numeroIngreso))# aca lo dejamos de menor a mayor
