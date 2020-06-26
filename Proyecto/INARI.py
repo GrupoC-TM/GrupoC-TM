8514d1e5b61a4d922c45a3463b151cb4ff258d91
sebastian <sebastianmauriz@gmail.com>
2020-06-26 12:32:40 -0300

    agrego parte del main y tambien la funcion de vendedor con su ventana

--- Proyecto/INARI.py
+++ Proyecto/INARI.py
@@ -1,4 +1,4 @@
-rom Tkinter import *
+from Tkinter import *
 import tkMessageBox
 import sqlite3
 
@@ -53,3 +53,87 @@ def cancelar_pedido():
         Button_1.grid(row=4,column=2,padx=10,pady=10)
 
         root.mainloop()
+
+#esta funcion destruye ventanas,se pasa la ventana por parametro
+def destruir_ventana(root):
+        root.destroy()
+
+
+def vendedor():
+        #configuraciones basicas de la ventana
+        root=Toplevel()
+        root.configure(background="Orange red")
+        RTitle=root.title("Vendedor")
+        RWidth=900
+        RHeight=500
+        root.geometry(("%dx%d")%(RWidth,RHeight))
+
+        #Label titulo
+        LabelTitulo = Label(root,text="INARI ADMIN",font=("AndaleMono",35,"bold"))
+        LabelTitulo.grid(row=0,column=2)
+
+        #variables comando para ejecutar ventanas
+        command=lambda :Ordenar_pedido()
+        command1=lambda :cancelar_pedido()
+        command2=lambda :pedidos_realizados()
+        command3=lambda :ordenes_canceladas()
+        command4=lambda :destruir_ventana(root)
+
+        #activebackground="DeepSkyBlue3",relief=SUNKEN,font=("AndaleMono",14,"bold"),bg="white",fg="black",borderwidth=4,width=15,height=5,
+        #crear botones
+        Button_1=Button(root,text="Nuevo pedido",activebackground="DeepSkyBlue3",relief=SUNKEN,font=("AndaleMono",12,"bold"),bg="white",fg="black",borderwidth=4,width=15,height=5,command=command)
+        Button_2=Button(root,text="Cancelar pedido",activebackground="DeepSkyBlue3",relief=SUNKEN,font=("AndaleMono",12,"bold"),bg="white",fg="black",borderwidth=4,width=15,height=5, command=command1)
+        Button_3=Button(root,text="Pedidos\n realizados",activebackground="DeepSkyBlue3",relief=SUNKEN,font=("AndaleMono",12,"bold"),bg="white",fg="black",borderwidth=4,width=15,height=5,command=command2)
+        Button_4=Button(root,text=" Pedidos\n Cancelados",activebackground="DeepSkyBlue3",relief=SUNKEN,font=("AndaleMono",12,"bold"),bg="white",fg="black",borderwidth=4,width=15,height=5,command=command3)
+
+        #botones nuevos,hay que agregarles las variales commando
+        Button_5=Button(root,text="Mostrar Caja",activebackground="DeepSkyBlue3",relief=SUNKEN,font=("AndaleMono",12,"bold"),bg="white",fg="black",borderwidth=4,width=15,height=5)
+        Button_6=Button(root,text="  Despachar \npedidos",activebackground="DeepSkyBlue3",relief=SUNKEN,font=("AndaleMono",12,"bold"),bg="white",fg="black",borderwidth=4,width=15,height=5)
+
+
+        Button_7=Button(root,text="Salir",activebackground="DeepSkyBlue3",relief=SUNKEN,font=("AndaleMono",12,"bold"),bg="white",fg="black",borderwidth=4,width=5,height=2,command=command4)
+
+        #posicionar botones
+        Button_1.grid(row=1,column=1,padx=60,pady=70)
+        Button_2.grid(row=1,column=2,padx=60,pady=70)
+        Button_3.grid(row=1,column=3,padx=60,pady=70)
+        Button_4.grid(row=2,column=1,padx=60,pady=5)
+        Button_5.grid(row=2,column=2,padx=60,pady=5)
+        Button_6.grid(row=2,column=3,padx=60,pady=5)
+
+        #boton salir
+        Button_7.grid(row=0,column=3,padx=60,pady=10)
+        root.mainloop()
+
+#Ventana Main
+def main():
+        root=Tk()
+        root.configure(background="orange red")
+        RTitle=root.title("Delivery Sushi")
+
+        #Label titulo
+        LabelTitulo = Label(root,text="INARI SUSHI",font=("AndaleMono",50,"bold"))
+        #LabelTitulo.grid(row=0,column=2,padx=3,pady=10)
+        LabelTitulo.place(x= 140,y =30)
+
+        #asignan variables de ancho y alto y se colocan en geometry
+        RWidth=700
+        RHeight=400
+        root.geometry(("%dx%d")%(RWidth,RHeight))
+
+        #variables comandos que ejecutan ventanas
+        command=lambda :vendedor()
+        command_1=lambda :cliente()
+
+        #botones
+        Button_1=Button(root,text="Vendedor",activebackground="DeepSkyBlue3",relief=SUNKEN,font=("AndaleMono",14,"bold"),bg="white",fg="black",borderwidth=4,width=15,height=5,command=command)
+        Button_2=Button(root,text="Cliente",activebackground="khaki1",relief=SUNKEN,font=("AndaleMono",14,"bold"),bg="white",fg="black",borderwidth=4,width=15,height=5,command=command_1)
+        #Button_1.grid(row=1,column=1,padx=5,pady=140)
+        #Button_2.grid(row=1,column=2,padx=6,pady=140)
+        Button_1.place(x= 50,y =180)
+        Button_2.place(x=460,y =180)
+
+
+        root.mainloop()
+
+main()
